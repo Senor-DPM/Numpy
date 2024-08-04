6d555dae2e93a148747b3a9a2c5c6a014351a403
Senor-DPM <dhruvmukherjee85@gmail.com>
2024-08-04 13:52:54 +0530

    Finished "Creating Arrays" topic in NumPy.

---
+++ 1_IntroductionArrays/1_creatingArrays.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# To create an array, simply use the array() method on a list or tuple. It's class is "numpy.ndarray".
+
+arrL = np.array([1,2,3,4,5])
+print(arrL, "\n", type(arrL), "\n")
+arrT = np.array((1,2,3,4,5))
+print(arrT, "\n", type(arrT), "\n")
+
+# Dimensions of an array can be defined as the levels of depth (nested arrays).
+# Each element of an array is a 0D array.
+
+arr1 = np.array([1,2,3,4,5]) # 1D Array
+arr2 = np.array([[1,2,3],[4,5,6]]) # 2D Array
+arr3 = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+print(arr1, arr2, arr3, sep = "\n\n")
+
+# To check the dimensions of an array, you can use the ndim method.
+
+print(arr1.ndim, arr2.ndim, arr3.ndim)
+
+# To create an n-dimension array, you can use the ndmin attribute
+
+arr4 = np.array([1, 2, 3, 4], ndmin = 5)
+print(arr4)
+print("No. of Dimension = ", arr4.ndim)
